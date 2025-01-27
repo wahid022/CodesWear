1b8f8047274cbbaae7c2928bb181350dfbbf8bc1
wahid022 <rahmanwahid022@gmail.com>
2025-01-27 12:41:41 +0530

    added mongoDb Schema for product,order and user

---
+++ models/User.js
@@ -0,0 +1,13 @@
+import { unique } from 'next/dist/build/utils';
+
+const mongoose =require('mongoose');
+
+
+const UserSchema=new mongoose.Schema({
+    username:{type:String,required:true},
+    email : {type:String,required:true,unique:true},
+    password : {type:String,required:true},
+    
+},{timestamps:true})
+
+export default mongoose.model("User",UserSchema);
